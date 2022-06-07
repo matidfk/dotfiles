554f821ff378e30198dfb0cf6c9a9dc4095b99ad
mat <72168331+matidfk@users.noreply.github.com>
2022-06-08 00:57:58 +0100

    adjusted top_padding

--- Themes/Catppuccin/bspwm.sh
+++ Themes/Catppuccin/bspwm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 bspc config border_width 0
 bspc config window_gap 12
-bspc config top_padding 40 # 28 + 12
+bspc config top_padding 54 # 30 + 12*2
 bspc config -m VGA-0 top_padding 0
--- Themes/Glassmorphism/bspwm.sh
+++ Themes/Glassmorphism/bspwm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 bspc config border_width 0
 bspc config window_gap 20
-bspc config top_padding 55 # 35 + 20
+bspc config top_padding 65
 bspc config -m VGA-0 top_padding 0
