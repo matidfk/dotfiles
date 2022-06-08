48163d0ef6e7964dd84d5f17cf5d3c3e028e2f0f
mat <72168331+matidfk@users.noreply.github.com>
2022-06-08 01:05:42 +0100

    changed top_padding

--- Themes/Catppuccin/bspwm.sh
+++ Themes/Catppuccin/bspwm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 bspc config border_width 0
 bspc config window_gap 12
-bspc config top_padding 54 # 30 + 12*2
+bspc config top_padding 52 # 30 + 10 + 12
 bspc config -m VGA-0 top_padding 0
