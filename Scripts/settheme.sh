183b10a2f0bb82f3fe75c1f4e60bb64daa6bd878
matidfk <72168331+matidfk@users.noreply.github.com>
2022-09-28 15:56:43 +0100

    changed how gtk themes work

--- Scripts/settheme.sh
+++ Scripts/settheme.sh
@@ -17,8 +17,12 @@ fi
 cd $THEMES_DIR/$1
 message=$(
     cp "dunstrc" "$HOME/.config/dunst/dunstrc" 2>&1
-    cp "gtk3.ini" "$HOME/.config/gtk-3.0/settings.ini" 2>&1
-    cp "gtk4.ini" "$HOME/.config/gtk-4.0/settings.ini" 2>&1
+#    cp "gtk3.ini" "$HOME/.config/gtk-3.0/settings.ini" 2>&1
+#    cp "gtk4.ini" "$HOME/.config/gtk-4.0/settings.ini" 2>&1
+    sudo rm -r "/usr/share/themes/Default/gtk-2.0" 2>&1
+    sudo rm -r "/usr/share/themes/Default/gtk-3.0" 2>&1
+    sudo cp -r "gtk-3.0" "/usr/share/themes/Default" 2>&1
+    sudo cp -r "gtk-2.0" "/usr/share/themes/Default" 2>&1
     cp "kitty.conf" "$HOME/.config/kitty/theme.conf" 2>&1
     cp "picom.conf" "$HOME/.config/picom/picom.conf" 2>&1
     cp "polybar.ini" "$HOME/.config/polybar/config.ini" 2>&1
