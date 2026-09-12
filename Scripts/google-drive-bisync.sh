2bf45974c6dc8633d2c400a76198d8c7b6aad217
mat <matidfk4@gmail.com>
2026-09-12 14:41:06 +0200

    googie drive changes

--- Scripts/google-drive-bisync.sh
+++ Scripts/google-drive-bisync.sh
@@ -1,4 +1,8 @@
+#!/bin/sh
 # touch /tmp/.google_drive_lock
+
+# exit if rclone is already running
+pgrep -x "rclone" > /dev/null && exit 0
 notify-send "Rclone" "Syncing google drive..." --app-name="Rclone" --icon=folder-google-drive
 rclone bisync /mnt/google_drive googledrive: --progress --verbose "$@" 2>&1
 # notify-send "Rclone" "$output" --app-name="Rclone" --icon=folder-google-drive
